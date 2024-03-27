ee124b2d622ed95f67b4067678f36a1b4b91cec7
Ashish Bhosle <asbhosle2003@gmail.com>
2024-03-26 20:32:45 -0700

    new commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 
-import {comparePassword, hashPassword } from "./../helpers/authHelper.js";
+import {comparePassword, hashPassword } from "./../controllers/helpers/authHelper.js";
 import JWT from "jsonwebtoken";
 
 export const registerController = async (req,res) => {
@@ -113,6 +113,45 @@ export const loginController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error in login",
+      error,
+    });
+  }
+};
+
+//forgotPasswordController
+
+export const forgotPasswordController = async (req, res) => {
+  try {
+    const { email, answer, newPassword } = req.body;
+    if (!email) {
+      res.status(400).send({ message: "Emai is required" });
+    }
+    if (!answer) {
+      res.status(400).send({ message: "answer is required" });
+    }
+    if (!newPassword) {
+      res.status(400).send({ message: "New Password is required" });
+    }
+    //check
+    const user = await userModel.findOne({ email, answer });
+    //validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Wrong Email Or Answer",
+      });
+    }
+    const hashed = await hashPassword(newPassword);
+    await userModel.findByIdAndUpdate(user._id, { password: hashed });
+    res.status(200).send({
+      success: true,
+      message: "Password Reset Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
       error,
     });
   }
