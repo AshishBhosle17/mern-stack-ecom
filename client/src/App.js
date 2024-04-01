dddac07a9386fe3fd733135f804d42511ec83206
Ashish Bhosle <asbhosle2003@gmail.com>
2024-04-01 00:33:44 -0700

    new commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,25 @@ import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
 import ForgotPasssword from "./pages/Auth/ForgotPassword";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import AdminRoute from "./components/Routes/AdminRoute";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<HomePage />} />
+      
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+        </Route>
+        
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPasssword />} />
         <Route path="/login" element={<Login/>} />
