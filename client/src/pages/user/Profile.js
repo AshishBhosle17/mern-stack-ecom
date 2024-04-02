05c95be3676e2cca09f2a63f59013a079889ec16
Ashish Bhosle <asbhosle2003@gmail.com>
2024-04-02 23:53:26 +0530

    commit

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -11,7 +11,7 @@ const Profile = () => {
             <UserMenu/>
             </div>
             <div className="col-md-9">
-              <h1 className="text-center">Profile</h1>
+              <h1 className="text-center">Pro</h1>
             </div>
         </div>
     </div>
