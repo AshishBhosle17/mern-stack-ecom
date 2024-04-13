e861b45e11ef0b333311286cebba687b597f7800
Ashish Bhosle <asbhosle2003@gmail.com>
2024-04-13 23:40:07 +0530

    commit

--- client/src/pages/Auth/HomePage.js
+++ client/src/pages/Auth/HomePage.js
@@ -1,24 +1,46 @@
 import React, { useState, useEffect } from "react";
-
+import { useNavigate } from "react-router-dom";
 import { Checkbox, Radio } from "antd";
 import axios from "axios";
 import toast from "react-hot-toast"
 import Layout from "../../components/Layout/Layout";
 import { useAuth } from "../../context/auth";
+import "../../styles/Homepage.css";
+import { Prices } from "../../components/Prices";
 
 const HomePage = () => {
+  const navigate = useNavigate;
   const [auth, setAuth] = useAuth();
   const [products, setProducts] = useState([]);
+  const [radio, setRadio] = useState([]);
+  const [total, setTotal] = useState(0);
   const [categories, setCategories] = useState([]);
 
-  //getall products
+  //get all cat
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:8080//api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  
+  }, []);
+
+  //get products
   const getAllProducts = async () => {
     try {
       const { data } = await axios.get("http://localhost:8080/api/v1/product/get-product");
       setProducts(data.products);
     } catch (error) {
       console.log(error);
-      toast.error("Someething Went Wrong");
+      toast.error("Something Went Wrong");
     }
   };
 
@@ -50,12 +72,17 @@ const HomePage = () => {
               </Checkbox>
             ))}
              </div>
-          {/* price filter */}
-          <h4 className="text-center mt-4">Filter By Price</h4>
+         {/* price filter */}
+         <h4 className="text-center mt-4">Filter By Price</h4>
           <div className="d-flex flex-column">
-            
+            <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+              {Prices?.map((p) => (
+                <div key={p._id}>
+                  <Radio value={p.array}>{p.name}</Radio>
+                </div>
+              ))}
+            </Radio.Group>
               
-            
           </div>
           <div className="d-flex flex-column">
             <button
@@ -72,7 +99,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
